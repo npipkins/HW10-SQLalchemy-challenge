8d68de7c0eb9c2f11faccd40a402bf497eb969af
npipkins <68622679+npipkins@users.noreply.github.com>
2020-10-29 17:53:25 -0500

    save

--- app.py
+++ app.py
@@ -28,12 +28,22 @@ def home():
     return (
         f"Welcome!<br/>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/tobs<br/>"
     )
 
-@app.route("/api/v1.0/precipitation")
+
+@app.route("/stations")
+def stations():
+    results = session.query(Station.name).all()
+
+    results_list = list(results)
+
+    return jsonify(results_list)
+    
+
+@app.route("/precipitation")
 def precipitation():
     results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date > '2016-08-23').all()
 
@@ -41,29 +51,17 @@ def precipitation():
 
     return jsonify(results_dict)
 
-@app.route("/api/v1.0/stations")
-def stations():
-    results = session.query(Station.name).all()
 
-    results_list = list(results)
 
-    return jsonify(results_list)
-
-@app.route("/api/v1.0/tobs")
+@app.route("/tobs")
 def tobs():
     most_active_id = 'USC00519281'
     results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= '2016-08-18').filter(Measurement.station == most_active_id).all()
+    
     results_list = list(results)
+    
     return jsonify(results_list)
 
-@app.route("/api/v1.0/<start>")
-def start(start):
-    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
-    results_list = list(results)
-    return jsonify(results_list)
 
-@app.route("/api/v1.0/<start>/<end>")
-def start_end(start, end):
-    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
-    results_list = list(results)
-    return jsonify(results_list)
+
+
